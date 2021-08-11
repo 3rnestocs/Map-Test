68d02e37811fa90cd790a76cb2bb9649ec9b0148
3rnestocs <3rnestocs@gmail.com>
2021-08-10 20:48:22 -0400

    Initial project setup without storyboard

---
+++ mapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapTest
+//
+//  Created by Ernesto Jose Contreras Lopez on 8/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setup()
+    }
+
+    private func setup() {
+        self.setupUI()
+    }
+
+    private func setupUI() {
+        self.view.backgroundColor = .gray
+    }
+}
+
